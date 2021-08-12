26ad943332cd88410908a44296bc2451e51b84d2
Nelson Neoh <nelson.neoh@webnic.cc>
2021-08-12 16:08:06 +0800

    Amend Verify script to accept args.

--- docker/sample_scripts/Verify-CSR.KEY.CRT.sh
+++ docker/sample_scripts/Verify-CSR.KEY.CRT.sh
@@ -3,14 +3,24 @@
 #if only domain certificate not match, need to reissue domain certificate.
 #if non matched, regenerate CSR and PrivateKey then reissue application again.
 
-#To view the md5 hash of the modulus of the private key:
-echo -n "KEY : " 
-openssl rsa -noout -modulus -in hualientour.com.tw.pem | openssl md5
+if [ $# -lt 3 ]
+then
+        echo
+        echo "Verify CSR / KEY / Domain CRT"
+        echo "Usage: ./Verify-CSR.KEY.CRT.sh csr_filename.csr privateKey_filename.key domain.crt"
+        echo
+        exit
+fi
+
 
 #To view the md5 hash of the modulus of the CSR:
 echo -n "CSR : " 
-openssl req -noout -modulus -in hualientour.com.tw.csr | openssl md5
+openssl req -noout -modulus -in $1 | openssl md5
+
+#To view the md5 hash of the modulus of the private key:
+echo -n "KEY : " 
+openssl rsa -noout -modulus -in $2 | openssl md5
 
 #To view the md5 hash of the modulus of the certificate:
 echo -n "CRT : " 
-openssl x509 -noout -modulus -in 525925741.crt | openssl md5
+openssl x509 -noout -modulus -in $3 | openssl md5
